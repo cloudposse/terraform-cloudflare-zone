3076ff818f45c8822766e7ff734665ba6e38dda5
SweetOps <vladimir.fess@gmail.com>
2021-03-16 19:10:37 +0200

    upd

--- firewall.tf
+++ firewall.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_filter" "default" {
+  for_each    = var.filters # list of filters with additional `action` files for cloudflare_firewall_rule
   zone_id     = join("", cloudflare_zone.default.*.id)
   description = "Wordpress break-in attempts that are outside of the office"
   expression  = "(http.request.uri.path ~ \".*wp-login.php\" or http.request.uri.path ~ \".*xmlrpc.php\") and ip.src ne 192.0.2.1"
