d762d069b75529a2f290e3af07f484c9cbc80a06
Benjamin Smith <ben.smith.developer@gmail.com>
2023-07-12 09:00:32 -0700

    update require version of TF

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 3.23"
+      version = ">= 4.0"
     }
     time = {
       source  = "hashicorp/time"
