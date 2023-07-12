1661ec5219f426406ab42a007c7128da15d4c5f0
Benjamin Smith <ben.smith.developer@gmail.com>
2023-07-12 09:13:41 -0700

    update versions

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.0"
+      version = ">= 3.23"
     }
     time = {
       source  = "hashicorp/time"
