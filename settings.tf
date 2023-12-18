c86142b54f51aea9512726a8d71d58c40719ec32
Vlad Korkach <v.korkach@playson.com>
2023-12-18 18:15:05 +0200

    feat(): update new version by DevOps

--- settings.tf
+++ settings.tf
@@ -7,8 +7,9 @@ resource "cloudflare_zone_settings_override" "this" {
   zone_id = local.zone_id
 
   settings {
-    always_use_https = lookup(each.value, "always_use_https", "on")
-    ssl              = lookup(each.value, "ssl", "full")
-    prefetch_preload = lookup(each.value, "prefetch_preload", null)
+    always_use_https  = lookup(each.value, "always_use_https", "on")
+    ssl               = lookup(each.value, "ssl", "full")
+    prefetch_preload  = lookup(each.value, "prefetch_preload", null)
+    browser_cache_ttl = lookup(each.value, "browser_cache_ttl", "0")
   }
 }
