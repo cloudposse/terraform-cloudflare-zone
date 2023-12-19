7346fb97304809e0b497cd3be3a62b5db1b2dbec
Vlad Korkach <v.korkach@playson.com>
2023-12-19 15:29:11 +0200

    feat(): update new version by DevOps

--- ruleset.tf
+++ ruleset.tf
@@ -32,9 +32,7 @@ resource "cloudflare_ruleset" "default" {
       dynamic "action_parameters" {
         for_each = lookup(rules.value, "action_parameters", null) == null ? [] : [rules.value.action_parameters]
 
-        content {
-          origin_error_page_passthru = lookup(action_parameters.value, "origin_error_page_passthru", null)
-        }
+        content {}
       }
 
       expression  = lookup(rules.value, "expression", null)
