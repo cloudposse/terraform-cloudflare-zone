2e1e0dfd0a88774e96767bf241408ea00186b013
Vlad Korkach <v.korkach@playson.com>
2023-12-19 13:17:07 +0200

    feat(): update new version by DevOps

--- ruleset.tf
+++ ruleset.tf
@@ -30,7 +30,7 @@ resource "cloudflare_ruleset" "default" {
       }
 
       dynamic "action_parameters" {
-        for_each = lookup(rule, "action_parameters", {})
+        for_each = lookup(rules, "action_parameters", {})
         # for_each = lookup(rules.value, "action_parameters", null) == null ? [] : [lookup(rules.value, "action_parameters", {})]
 
         content {
