43aa65a33f8f5bb9da2210053beebce2f0f7e742
Bryan Horstmann <bhorstmann@gmail.com>
2023-12-20 16:43:39 +0200

    Define pull page_rule spec

--- variables.tf
+++ variables.tf
@@ -123,11 +123,83 @@ variable "firewall_rules" {
 }
 
 variable "page_rules" {
-  type        = any
+  type = list(object({
+    target   = string
+    priority = optional(number)
+    status   = optional(string)
+    actions = object({
+      always_use_https         = optional(bool)
+      automatic_https_rewrites = optional(string)
+      browser_cache_ttl        = optional(number)
+      browser_check            = optional(string)
+      bypass_cache_on_cookie   = optional(string)
+      cache_by_device_type     = optional(string)
+      cache_deception_armor    = optional(string)
+      cache_key_fields = optional(object({
+        cookie = optional(object({
+          check_presence = optional(list(string))
+          include        = optional(list(string))
+        }))
+        header = optional(object({
+          check_presence = optional(list(string))
+          exclude        = optional(list(string))
+          include        = optional(list(string))
+        }))
+        host = object({
+          resolved = optional(bool)
+        })
+        query_string = object({
+          exclude = optional(list(string))
+          include = optional(list(string))
+          ignore  = optional(bool)
+        })
+      }))
+      cache_level     = optional(string)
+      cache_on_cookie = optional(string)
+      cache_ttl_by_status = optional(object({
+        codes = string
+        ttl   = number
+      }))
+      disable_apps           = optional(bool)
+      disable_performance    = optional(bool)
+      disable_railgun        = optional(bool)
+      disable_security       = optional(bool)
+      disable_zaraz          = optional(bool)
+      edge_cache_ttl         = optional(number)
+      email_obfuscation      = optional(string)
+      explicit_cache_control = optional(string)
+      forwarding_url = optional(object({
+        url         = string
+        status_code = number
+      }))
+      host_header_override = optional(string)
+      ip_geolocation       = optional(string)
+      minify = optional(object({
+        css  = string
+        html = string
+        js   = string
+      }))
+      mirage                      = optional(string)
+      opportunistic_encryption    = optional(string)
+      origin_error_page_pass_thru = optional(string)
+      polish                      = optional(string)
+      resolve_override            = optional(string)
+      respect_strong_etag         = optional(string)
+      response_buffering          = optional(string)
+      rocket_loader               = optional(string)
+      security_level              = optional(string)
+      server_side_exclude         = optional(string)
+      smart_errors                = optional(string)
+      sort_query_string_for_cache = optional(string)
+      ssl                         = optional(string)
+      true_client_ip_header       = optional(string)
+      waf                         = optional(string)
+    })
+  }))
   default     = null
   description = <<-DOC
   A list of maps of Page Rules.
   The values of map is fully compliant with `cloudflare_page_rule` resource.
-  To get more info see https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/cloudflare_page_rule
+  To get more info see https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/page_rule
   DOC
 }
