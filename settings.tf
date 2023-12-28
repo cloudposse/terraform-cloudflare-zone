19a2eef22556435fd95cbcf73007a6652f6b8389
Vlad Korkach <v.korkach@playson.com>
2023-12-28 12:13:38 +0200

    feat(): update new version by DevOps

--- settings.tf
+++ settings.tf
@@ -1,14 +1,21 @@
 # locals {
-#   settings = { for sett in var.settings : local.zone_id => sett }
+#   settings = module.this.enabled && var.settings != [] ? {
+#     for sett in flatten(var.settings) :
+#     local.zone_id => sett
+#   } : {}
 # }
+
 locals {
-  settings = module.this.enabled && var.settings != [] ? {
-    for sett in flatten(var.settings) :
-    local.zone_id => sett
+  settings = module.this.enabled && var.settings != null ? {
+    for zone_id, sett in var.settings :
+    zone_id => {
+      always_use_https  = lookup(sett, "always_use_https", "on")
+      ssl               = lookup(sett, "ssl", "full")
+      prefetch_preload  = lookup(sett, "prefetch_preload", null)
+      browser_cache_ttl = lookup(sett, "browser_cache_ttl", 14400)
+    }
   } : {}
 }
-
-
 resource "cloudflare_zone_settings_override" "this" {
   for_each = local.settings
 
